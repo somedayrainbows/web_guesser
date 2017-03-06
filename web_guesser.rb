d344bf420f1599432bbb23aa6abe0dcd2e1a7d35
somedayrainbows <ebassity@gmail.com>
2017-03-05 20:24:39 -0700

    I4 colors changing on output finished

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,25 +2,35 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 class GuessChecker
-  attr_reader :number
+  attr_reader :secret_number
 
   def initialize
-    @number = rand(100)
+    @secret_number = rand(100)
   end
 
   def check_guess(guess)
     if guess == nil
-      "Enter a number!"
-    elsif guess.to_i > number + 5
-      "Way too high!"
-    elsif guess.to_i > number
+      "Welcome to my 'Guess the Number' game. Enter a number to start playing!"
+    elsif guess.to_i > secret_number + 5
+      "Way too high! Guess again."
+    elsif guess.to_i > secret_number
       "Too high!"
-    elsif guess.to_i < number - 5
-      "Way too low!"
-    elsif guess.to_i < number
-      "Too low!"
-    else guess.to_i == number
-      "You got it right! The SECRET NUMBER is #{number}"
+    elsif guess.to_i < secret_number - 5
+      "Way too low! Guess again."
+    elsif guess.to_i < secret_number
+      "Too low! Guess again."
+    else guess.to_i == secret_number
+      "You got it right! The SECRET NUMBER is #{secret_number}."
+    end
+  end
+
+  def background_color(guess)
+    if guess.to_i < (secret_number - 5) || guess.to_i > (secret_number + 5)
+      color = "red"
+    elsif guess.to_i < secret_number || guess.to_i > secret_number
+      color = "pink"
+    else
+      color = "green"
     end
   end
 
@@ -29,5 +39,5 @@ end
 gc = GuessChecker.new
 get '/' do
   guess = params["guess"]
-  erb :index, :locals => {:message => gc.check_guess(guess)}
+  erb :index, :locals => {:message => gc.check_guess(guess), :color => gc.background_color(guess)}
 end
